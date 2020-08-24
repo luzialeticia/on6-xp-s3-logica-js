644e4b23af086e8522d8ecc262aca20127d59fac
luzialeticia <leticialuzia@tutamail.com>
2020-08-24 12:21:11 -0300

    Arquivo ex4

---
+++ Entrega/ex4.js
@@ -0,0 +1,2 @@
+/* 4. Crie uma função que receba um número e devolva uma soma progressiva.
+Por exemplo, recebendo o número 5, a função deve retornar 15, ou seja, o resultado de 1 + 2 + 3 + 4 + 5. */
