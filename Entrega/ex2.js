d262f2555940364429176664a5da0c9d3dfd70e9
luzialeticia <leticialuzia@tutamail.com>
2020-08-24 12:02:49 -0300

    Criando a função tempCelsius, a variável tempFahrenheit e o console.log que imprime as informações

--- Entrega/ex2.js
+++ Entrega/ex2.js
@@ -1,1 +1,11 @@
 // Agora faça o contrário, um programa que receba uma temperatura em Fahrenheit e chame uma função que converta para Celsius.
+
+const input = require('readline-sync')
+
+function tempCelsius(x) {
+    return (x-32) * 5/9
+}
+
+const tempFahrenheit = parseFloat(input.question("Digite a temperatura em Fahrenheit: "))
+
+console.log(`${tempFahrenheit}°C corresponde a ${tempCelsius(tempFahrenheit)}°F.`)
