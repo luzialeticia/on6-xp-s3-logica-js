e798d2455b3afb3089ee9e8652b0f57624060fe7
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 19:41:34 -0300

    Terminando a lógica do if else

--- Entrega/ex4.js
+++ Entrega/ex4.js
@@ -6,22 +6,24 @@
     Se todos os parâmetros forem passados, retorne a soma do primeiro com o segundo, e o resultado dividido pelo terceiro.
     Se nenhum argumento for passado, retorne "não recebeu parâmetro" */
 
-const input = require('readline-sync')
-
+    
 function calc(a, b, c) {
-    if(!b && !c) {
-        return a
-    }else if(!c) {
-            return a + b
-        }else if(!a && !b && !c) {
-                return "Não recebeu parâmetro"
-            }else {
-                return (a+b)/c
-            }
+    return (a+b)/c
 }
 
+const input = require('readline-sync')
+    
 const num1 = parseInt(input.question("Digite o 1º nº: "))
 const num2 = parseInt(input.question("Digite o 2º nº: "))
 const num3 = parseInt(input.question("Digite o 3º nº: "))
+    
 
-console.log(`${calc(num1, num2, num3)}`)
+if(!num2 && !num3) {
+    console.log(num1)
+    }else if(!num3) {
+        console.log(num1+num2)
+        }else if(!num1 && !num2 && !num3) {
+            console.log("Não recebeu parâmetro")
+            }else {
+                console.log(`${calc(num1, num2, num3)}`)
+            }
