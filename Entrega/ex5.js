613e5226fc0dbbcef14f7b6203e4e5e3638c3a7f
luzialeticia <leticialuzia@tutamail.com>
2020-08-26 21:20:21 -0300

    Colocando a função no início do código

--- Entrega/ex5.js
+++ Entrega/ex5.js
@@ -1,10 +1,6 @@
 /* 5. Crie uma função que receba um número e devolva uma soma progressiva.
 Por exemplo, recebendo o número 5, a função deve retornar 15, ou seja, o resultado de 1 + 2 + 3 + 4 + 5. */
 
-const input = require('readline-sync')
-
-let numero = parseInt(input.question("Digite um nº inteiro: "))
-
 function somaNumero(x) {
     let soma = 0
     for (let i = 0; i <= x; i++) {
@@ -12,4 +8,9 @@ function somaNumero(x) {
     }
     return soma
 }
+
+const input = require('readline-sync')
+
+let numero = parseInt(input.question("Digite um nº inteiro: "))
+
 console.log(somaNumero(numero))
