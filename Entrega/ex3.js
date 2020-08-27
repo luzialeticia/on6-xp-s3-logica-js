546d2d029079da4f32b295d6213ab6de60c60b8a
luzialeticia <leticialuzia@tutamail.com>
2020-08-27 12:45:24 -0300

    Colocando a function no início do código

--- Entrega/ex3.js
+++ Entrega/ex3.js
@@ -1,8 +1,9 @@
 // 3. Faça um programa que receba um valor em metros e e chame uma função que converta em milímetros.
 
+const milimetros = (x) => x*1000 //Arrow function
+
 const input = require('readline-sync')
 
 const metros = parseFloat(input.question("Digite o valor em metros: "))
-const milimetros = (x) => x*1000 //Arrow function
 
 console.log(`${metros} metros corresponde a ${milimetros(metros)} milímetros.`)
